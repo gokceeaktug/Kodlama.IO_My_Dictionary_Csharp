a35b3106eb4731dafdfa638287e4a5e134da730c
Gökçe Aktuğ <gokceeaktug@gmail.com>
2021-02-06 18:42:42 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<double, string> myDictonary = new MyDictionary<double, string>();
+
+            myDictonary.Add(12456391101, "Gökçe Aktuğ");
+            myDictonary.Add(12456391102, "Büşra Ayverdi");
+            myDictonary.Add(12456391103, "Ezgi Aslan");
+            myDictonary.Add(10000000001, "Büşra Özen");
+            myDictonary.Add(10104007001, "Engin Demiroğ");
+
+            myDictonary.MyList();
+        }
+    }
+}
